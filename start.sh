adb9dc9f543fa96aca226d9b77ceb7c5a5f4731e
Kartik OP <85619363+IndianDevInc@users.noreply.github.com>
2022-06-09 19:41:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@ service ssh start
 service nginx start
 
 echo "set ngrok token: $NGROK_TOKEN"
-ngrok authtoken $NGROK_TOKEN
+./ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
-ngrok tcp 22 --log=stdout > ngrok.log
+./ngrok tcp 22 --log=stdout > ngrok.log
